b8944e4836afb11228026538ee8b76d48246d33c
project-3 <ulugbek.musayev906@gmail.com>
2021-12-26 01:16:26 +0500

    approved

--- src/App.js
+++ src/App.js
@@ -2,46 +2,59 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+const element = <FontAwesomeIcon icon={faCoffee} />;
 
 function App() {
   return (
     <div className="App">
-      <div className="container py-5">
+      <div className="container-fluid py-5">
         <div className="row d-flex justify-content-center">
-          <div className="col-md-4">
-            <div className="bg-white shadow pb-4 text-center">
-              <img
-                src="https://i1.wp.com/grrip.eu/wp-content/uploads/2021/04/ocean.jpeg?fit=889%2C667&ssl=1"
-                className="w-100 mb-3"
-                alt=""
-              />
-              <h1 className="mx-4">
-                Blue Ocean <br /> Waves Crush
-              </h1>
-              <p className="text-secondary mx-4">
+          <div className="col-md-3">
+            <div className="bg-dark shadow pb-4 text-center">
+              <h1 className="text-success">''</h1>
+              <p className="text-secondary mx-4 text-white">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
                 explicabo aliquam tempora vitae a doloremque quod, nemo at
                 ullam, quasi voluptate ratione porro eum odit suscipit quibusdam
                 dolorum, nisi corrupti?
               </p>
+              <img
+                src="https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg"
+                alt=""
+              />
+              <h5 className="mx-4 text-success">JOHN DOE</h5>
             </div>
           </div>
-          <div className="col-md-4">
-            <div className="bg-white shadow pb-4 text-center">
-              <img
-                src="https://cdn.britannica.com/65/161665-050-C4B1FA75/Waves-North-Shore-Oahu-Hawaiian-Islands.jpg"
-                className="w-100 mb-3"
-                alt=""
-              />
-              <h1 className="mx-4">
-                Blue Ocean <br /> Waves Crush
-              </h1>
-              <p className="text-secondary mx-4">
+          <div className="col-md-3">
+            <div className="bg-dark shadow pb-4 text-center">
+              <h1 className="text-success">''</h1>
+              <p className="text-secondary mx-4 text-white">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
                 explicabo aliquam tempora vitae a doloremque quod, nemo at
                 ullam, quasi voluptate ratione porro eum odit suscipit quibusdam
                 dolorum, nisi corrupti?
               </p>
+              <img
+                src="https://api.time.com/wp-content/uploads/2019/12/time-person-of-the-year-joe-biden-portrait.jpg?quality=85&crop=0px%2C42px%2C2057px%2C1373px&resize=407%2C271&strip"
+                alt=""
+              />
+              <h5 className="mx-4 text-success">JOHN DOE</h5>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="bg-dark shadow pb-4 text-center">
+              <h1 className="text-success">''</h1>
+              <p className="text-secondary mx-4 text-white">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
+                explicabo aliquam tempora vitae a doloremque quod, nemo at
+                ullam, quasi voluptate ratione porro eum odit suscipit quibusdam
+                dolorum, nisi corrupti?
+              </p>
+              <img
+                src="https://www.al.com/resizer/GT06i1VRWpqtgAHmxaLJc5sh2gw=/1280x0/smart/arc-anglerfish-arc2-prod-advancelocal.s3.amazonaws.com/public/WEDAUZS6UZGFDLZHWBUNPYYM4Q.jpg"
+                alt=""
+              />
+              <h5 className="mx-4 text-success">JOHN DOE</h5>
             </div>
           </div>
         </div>
